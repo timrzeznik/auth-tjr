828aa0baf74110d59be26406d035219afe6d270e
timrzeznik <tim.rzeznik@kellogg.northwestern.edu>
2022-02-24 01:14:49 +0000

    version 7:14pm

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,10 @@ class SessionsController < ApplicationController
         @user = User.where({ email: params["email"] })[0]
         if @user
             if  @user.password == params["password"]
+                flash[:notice] = "You logged in successfully!"
                 redirect_to "/companies"
             else
+                flash[:notice] = "Nope"
                 redirect_to "/sessions/new"
             end
         else
